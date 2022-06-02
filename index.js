ef5951c5ed668903970ab7e3caa1e356873d6bd8
SadiLuis <84958501+SadiLuis@users.noreply.github.com>
2022-06-01 22:52:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://chat-app-react18.netlify.app",
         methods: ["GET", "POST"],
     }
 });
